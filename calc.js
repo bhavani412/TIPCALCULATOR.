9b9f31716fc1844b7d326afc7dcabb73625435d2
bhavani412 <77038804+bhavani412@users.noreply.github.com>
2021-11-24 21:55:44 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,43 @@
+function tip5(){
+    var a = document.getElementById("billamnt").value;
+    var b = document.getElementById("numpersons").value;
+    var c = 5;
+    var d = (a/b)*(c/100);
+    document.getElementById("tipamnt").value = d;
+    document.getElementById("total").value = ((a/b)+d);
+}
+function tip10(){
+    var a = document.getElementById("billamnt").value;
+    var b = document.getElementById("numpersons").value;
+    var c = 10;
+    var d = (a/b)*(c/100);
+    document.getElementById("tipamnt").value = d;
+    document.getElementById("total").value = ((a/b)+d);
+}
+function tip15(){
+    var a = document.getElementById("billamnt").value;
+    var b = document.getElementById("numpersons").value;
+    var c = 15;
+    var d = (a/b)*(c/100);
+    document.getElementById("tipamnt").value = d;
+    document.getElementById("total").value = ((a/b)+d);
+}
+function tip20(){
+    var a = document.getElementById("billamnt").value;
+    var b = document.getElementById("numpersons").value;
+    var c = 20;
+    var d = (a/b)*(c/100);
+    document.getElementById("tipamnt").value = d;
+    document.getElementById("total").value = ((a/b)+d);
+}
+function tip25(){
+    var a = document.getElementById("billamnt").value;
+    var b = document.getElementById("numpersons").value;
+    var c = 25;
+    var d = (a/b)*(c/100);
+    document.getElementById("tipamnt").value = d;
+    document.getElementById("total").value = ((a/b)+d);
+}
+function reset(){
+    var a=document.getE
+}
